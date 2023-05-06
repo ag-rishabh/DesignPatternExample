b1527d1700321b071139af337fe6d9abb92ec71b
rishbah <agrishabh.ag@gmail.com>
2023-05-06 23:46:46 +0530

    Added Builder Design Pattern Code

---
+++ DesignPattern/BuilderDP/User.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.BuilderDP
+{
+    public class User
+    {
+        private string _name;
+        private string _email;
+        private int _age;
+        private string _address;
+
+        private User() { }
+
+        public string Name { get { return _name; } }
+        public string Email { get { return _email; } }
+        public int Age { get { return _age; } }
+        public string Address { get { return _address; } }
+
+
+        public class UserBuilder
+        {
+            private readonly User user;
+
+            public UserBuilder()
+            {
+                user = new User();
+            }
+
+            public UserBuilder SetName(string name)
+            {
+                user._name = name;
+                return this;
+            }
+
+            public UserBuilder SetEmail(string email)
+            {
+                user._email = email;
+                return this;
+            }
+
+            public UserBuilder SetAge(int age) 
+            { 
+                user._age = age; 
+                return this; 
+            }
+
+            public UserBuilder SetAddress(string address)
+            {
+                user._address = address;
+                return this;
+            }
+
+            public User Build()
+            {
+                Console.WriteLine("User object is created");
+                return user;
+            }
+        }
+
+    }
+}
--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using DesignPattern;
 using DesignPattern.AbstractFactoryDP;
+using DesignPattern.BuilderDP;
 using DesignPattern.FactoryDP;
 
 //SingletonDP singleton = SingletonDP.GetInstance();
@@ -13,8 +14,14 @@ using DesignPattern.FactoryDP;
 
 //Pizza Cheese = PizzaStore.orderPiza(PizzaType.Cheese);
 
-IFactory win = GUIAbstractFactory.CreateFactory("Win");
-IButton button = win.CreateButton();
-button.PressButton();
-ITextBox textBox = win.CreateTextBox();
-textBox.ShowTextBox();
+//IFactory win = GUIAbstractFactory.CreateFactory("Win");
+//IButton button = win.CreateButton();
+//button.PressButton();
+//ITextBox textBox = win.CreateTextBox();
+//textBox.ShowTextBox();
+
+User.UserBuilder builder = new User.UserBuilder();
+builder.SetName("Rishabh").SetEmail("rishabh@gmail.com").SetAge(27);
+
+User user = builder.Build();
+Console.WriteLine(user.Name);
